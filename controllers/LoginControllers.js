8a56937d9231b14ace910cfed03acdb7b0c6b6df
johnmn007 <afheryita@gmail.com>
2025-11-24 21:15:19 -0500

    arregle bcrypt por bcryptjs

--- controllers/LoginControllers.js
+++ controllers/LoginControllers.js
@@ -1,5 +1,5 @@
 const { Login } = require("../db");
-const  bcrypt  =  require ( 'bcrypt' ) ; 
+const  bcrypt  =  require ( 'bcryptjs' ) ; 
 
 
 const createLogin = async (idRol, usuario, contraseña, responsable) => {
